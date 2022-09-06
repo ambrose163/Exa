14d7365502ccb687ab4877c6dbb88b5a41c841b3
Dmitriy <ambrose163@mail.ru>
2022-09-06 12:21:03 +0400

    save3

--- example002/Program.cs
+++ example002/Program.cs
@@ -1,3 +1,6 @@
-﻿string username = Console.ReadLine();
-Console.WriteLine("Привет,");
+﻿Console.WriteLine("Введите ваше имя");
+string? username = Console.ReadLine ();
+if (username == null)
+Console.WriteLine ("Нельзя ввести null");
+Console.WriteLine("Привет, ");
 Console.WriteLine(username);
